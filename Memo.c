9d5e910e3f567e78e6db6cd968c1b5b7beb40197
Alfonce Morara <alfoncemorara412@gmail.com>
2023-07-26 22:03:43 +0300

    Create Memo.c

---
+++ Memo.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+/**
+ * bfree -  NULLs the address and frees a pointer 
+ * @ptr: address of the pointer to free
+ *
+ * Return: 1 if freed, otherwise 0.
+ */
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
