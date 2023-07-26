78dd55b6821fe62edf4628116e1068d47fa303d2
Alfonce Morara <alfoncemorara412@gmail.com>
2023-07-26 22:31:53 +0300

    Create pointer.c

---
+++ pointer.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+ * _strlen - outputs length of a string
+ * @s:  string whose length to check
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strcmp -  lexicogarphic comparison of two strangs.
+ * @s1:  first strang
+ * @s2: second strang
+ *
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * starts_with - identifies if needle starts with haystack
+ * @haystack: string to search
+ * @needle:  substring to find
+ *
+ * Return:   NULL or address of next char of haystack
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @des destination buffer
+ * @src:source buffer
+ *
+ * Return: pointer to destination buffer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
