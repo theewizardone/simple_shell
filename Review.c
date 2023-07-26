714439c731077c196a0c45903b8fc9ba20a53129
Alfonce Morara <alfoncemorara412@gmail.com>
2023-07-26 21:32:23 +0300

    Create Review.c

---
+++ Review.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ **_strncpy - copies a str
+ *@dest: the destination str to be copied to
+ *@src: the source string
+ *@n: the amount of char to be replicated
+ *Return: the concatenated str
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat -  two strings concatenates
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be fully used
+ *Return: the concatenated str
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - identifies a char in a str
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s)  pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
